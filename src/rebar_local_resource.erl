e871ce51199f7243933607c8b07dd77fa28b547f
yqfclid <1192918988@qq.com>
2020-09-23 16:54:43 +0800

    update

--- src/rebar_local_resource.erl
+++ src/rebar_local_resource.erl
@@ -1,15 +1,15 @@
 -module(rebar_local_resource).
 
--behaviour(rebar_resource).
+-behaviour(rebar_resource_v2).
 
--export([init/1
+-export([init/2
     ,lock/2
-    ,download/3
+    ,download/4
     ,needs_update/2
-    ,make_vsn/1]).
+    ,make_vsn/2]).
 
--spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
-init(State) ->
+-spec init(atom(), rebar_state:t()) -> {ok, rebar_state:t()}.
+init(_Dir, State) ->
     {ok, rebar_state:add_resource(State, {local, rebar_local_resource})}.
 
 lock(_Dir, SourcePath) ->
@@ -18,9 +18,9 @@ lock(_Dir, SourcePath) ->
 needs_update(_Dir, {local, _SourcePath}) ->
     true.
 
-download(Dir, {local, SourcePath}, _State) ->
+download(Dir, {local, SourcePath}, _State, _) ->
     ok = rebar_file_utils:cp_r(filelib:wildcard(SourcePath ++ "/*"), Dir),
     {ok, undefined}.
 
-make_vsn(_Dir) ->
+make_vsn(_AppInfo, _Dir) ->
     {plain, "unused"}.
